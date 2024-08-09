b621702486215a855567db2c01f5e9c92214be2d
Hong An <nguyenhongan.tphcm@gamil.com>
2024-08-09 10:02:16 +0700

    First Code Push

---
+++ PraticeDbFirst2/Data/KhachHang.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PraticeDbFirst2.Data;
+
+public partial class KhachHang
+{
+    public string MaKh { get; set; } = null!;
+
+    public string TenCty { get; set; } = null!;
+
+    public string? DiaChi { get; set; }
+
+    public string? DienThoai { get; set; }
+
+    public int ThanhPho { get; set; }
+
+    public virtual ICollection<HoaDon> HoaDons { get; set; } = new List<HoaDon>();
+
+    public virtual ThanhPho ThanhPhoNavigation { get; set; } = null!;
+}
---
+++ PraticeDbFirst2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PraticeDbFirst2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
